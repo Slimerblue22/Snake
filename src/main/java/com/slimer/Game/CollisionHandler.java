477dff9ad00f88523281225f63f2fd0afc6b2d14
Slimer <legodude063@gmail.com>
2023-09-01 13:56:28 -0400

    Preparing for beta 1 of 2.0
    Added self collision checks

--- src/main/java/com/slimer/Game/CollisionHandler.java
+++ src/main/java/com/slimer/Game/CollisionHandler.java
@@ -1,9 +1,14 @@
 package com.slimer.Game;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+import java.util.List;
+
 /**
  * Manages collision detection logic for the snake game.
  * This class is responsible for detecting collisions with walls and checking the solidity of the block below the snake.
@@ -12,25 +17,28 @@ public class CollisionHandler {
     private final GameManager gameManager;
     private final Player player;
     private Location lastKnownLocation;
+
     /**
      * Constructs a new CollisionHandler.
      *
      * @param gameManager The GameManager instance to manage game logic.
-     * @param player The player for whom collision checks are to be performed.
+     * @param player      The player for whom collision checks are to be performed.
      */
     public CollisionHandler(GameManager gameManager, Player player) {
         this.gameManager = gameManager;
         this.player = player;
     }
+
     /**
      * Runs collision checks for the snake.
      * If a collision is detected, the game for the player is stopped.
      */
     public void runCollisionChecks() {
-        if (checkWallCollision() || checkSolidBlockBelow()) {
+        if (checkWallCollision() || checkSolidBlockBelow() || checkSelfCollision()) {
             gameManager.stopGame(player);  // End the game for the player
         }
     }
+
     /**
      * Checks for wall collisions by comparing the current and last known locations of the snake's head.
      *
@@ -53,12 +61,14 @@ public class CollisionHandler {
         Location roundedLocation = new Location(currentLocation.getWorld(), x, y, z);
 
         if (lastKnownLocation != null && lastKnownLocation.equals(roundedLocation)) {
+            Bukkit.getLogger().info("[CollisionHandler.java] Wall collision detected!");  // Debug line
             return true; // Collision detected
         } else {
             lastKnownLocation = roundedLocation; // No collision detected
             return false;
         }
     }
+
     /**
      * Checks if the block below the snake's head is solid.
      *
@@ -74,6 +84,49 @@ public class CollisionHandler {
         Location currentLocation = sheepEntity.getLocation();
         Block blockBelow = currentLocation.getWorld().getBlockAt(currentLocation.add(0, -1, 0));
 
-        return !blockBelow.getType().isSolid(); // True if the block below is not solid
+        if (!blockBelow.getType().isSolid()) {
+            Bukkit.getLogger().info("[CollisionHandler.java] Non-solid block below detected!");  // Debug line
+            return true; // True if the block below is not solid
+        }
+
+        return false; // False if the block below is solid
+    }
+
+    /**
+     * Checks for a collision between the snake's head and its segments.
+     *
+     * @return True if a self-collision is detected, false otherwise.
+     */
+    // NOTICE: There is a rare chance collision will not always flag when only the head and 1 segment exist.
+    // This bug is fairly rare and seemingly random.
+    // Until further information is obtained, this bug is to be ignored as it's very rare and does not cause major issues.
+    private boolean checkSelfCollision() {
+        SnakeCreation snake = gameManager.getSnakeForPlayer(player);
+        if (snake == null) {
+            return false;
+        }
+
+        Entity sheepEntity = snake.getSheepEntity();  // This is the head of the snake
+        Vector headLocation = sheepEntity.getLocation().toVector();
+
+        // Get all the segments of the snake
+        List<Entity> segments = gameManager.getSegmentsForPlayer(player);
+
+        // If there are no segments, ignore collision checks
+        if (segments.isEmpty()) {
+            return false;
+        }
+
+        // Iterate through the segments, starting from the first one
+        for (Entity segment : segments) {
+            Vector segmentLocation = segment.getLocation().toVector();
+            if (headLocation.distance(segmentLocation) < 0.1) {  // Tolerance of 0.1 blocks
+                Bukkit.getLogger().info("[CollisionHandler.java] Self-collision detected!");  // Debug line
+                return true;  // Self-collision detected
+            }
+        }
+
+
+        return false;  // No self-collision detected
     }
 }
