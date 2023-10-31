aa57a4965c690d33e1aea34deec7b3b3ea5264f0
Slimer <legodude063@gmail.com>
2023-10-31 12:45:41 -0400

    Code cleanup (PlayerInputHandler.java)
    Also includes a condition check to prevent checking players not in a game.

--- src/main/java/com/slimer/Game/PlayerInputHandler.java
+++ src/main/java/com/slimer/Game/PlayerInputHandler.java
@@ -16,6 +16,9 @@ import java.util.Random;
 
 /**
  * This class is responsible for handling player inputs for controlling snake movement.
+ * <p>
+ * Last updated: V2.1.0
+ * @author Slimerblue22
  */
 public class PlayerInputHandler {
     private final Plugin plugin;
@@ -28,23 +31,23 @@ public class PlayerInputHandler {
      * Constructs a PlayerInputHandler and registers the packet listener for key presses.
      *
      * @param plugin The Bukkit plugin.
+     * @param gameManager The game manager instance responsible for handling game logic.
      */
     public PlayerInputHandler(Plugin plugin, GameManager gameManager) {
         this.plugin = plugin;
-        new Vector();
         registerPacketListener();
         this.gameManager = gameManager;
     }
 
     /**
-     * Starts monitoring a player's direction based on input.
-     * Initializes the player's direction to a random direction.
+     * Starts monitoring a player's direction based on input and initializes it to a random direction.
      *
      * @param player The player to be monitored.
      */
     public void startMonitoring(Player player) {
         DebugManager.log(DebugManager.Category.PLAYER_INPUT, "Starting to monitor player: " + player.getName());
-        // Possible directions the snake can move
+
+        // Possible directions: North, South, East, West
         Vector[] possibleDirections = {
                 new Vector(0, 0, -1),  // North
                 new Vector(0, 0, 1),   // South
@@ -52,7 +55,7 @@ public class PlayerInputHandler {
                 new Vector(-1, 0, 0)   // West
         };
 
-        // Initialize player's direction to a random one
+        // Initialize to a random direction
         Vector randomDirection = possibleDirections[random.nextInt(possibleDirections.length)];
         playerDirections.put(player, randomDirection);
     }
@@ -79,7 +82,7 @@ public class PlayerInputHandler {
 
     /**
      * Registers a packet listener to handle incoming packets related to vehicle steering,
-     * which is used to detect the player's input direction.
+     * which is used to detect the player's input direction for snake movement.
      */
     private void registerPacketListener() {
         ProtocolLibrary.getProtocolManager().addPacketListener(
@@ -98,47 +101,59 @@ public class PlayerInputHandler {
      * @param event The PacketEvent containing the packet data.
      */
     private void handleKeyPress(PacketEvent event) {
-        if (event.getPacketType() == PacketType.Play.Client.STEER_VEHICLE) {
-            Player player = event.getPlayer();
-            float forward = event.getPacket().getFloat().read(1);
+        // Check if the packet is related to vehicle steering
+        if (event.getPacketType() != PacketType.Play.Client.STEER_VEHICLE) {
+            return;
+        }
 
-            // If W key is pressed, update the direction based on player's yaw
-            if (forward > 0) {
-                float yaw = player.getLocation().getYaw();
-                yaw = (yaw % 360 + 360) % 360;
-                Vector newDirection;
-                String facingDirection;
-                DebugManager.log(DebugManager.Category.PLAYER_INPUT, "Handling key press for player: " + event.getPlayer().getName());
+        Player player = event.getPlayer();
+        // Check if the player is in a game, if not, return early
+        if (gameManager.getSnakeForPlayer(player) == null) {
+            return;
+        }
 
-                if (yaw >= 315 || yaw < 45) {
-                    newDirection = new Vector(0, 0, 1);  // South
-                    facingDirection = "South";
-                } else if (yaw >= 45 && yaw < 135) {
-                    newDirection = new Vector(-1, 0, 0);  // East
-                    facingDirection = "East";
-                } else if (yaw >= 135 && yaw < 225) {
-                    newDirection = new Vector(0, 0, -1);  // North
-                    facingDirection = "North";
-                } else {  // yaw >= 225 && yaw < 315
-                    newDirection = new Vector(1, 0, 0);  // West
-                    facingDirection = "West";
-                }
-                playerDirections.put(player, newDirection);
-                DebugManager.log(DebugManager.Category.PLAYER_INPUT, "Player: " + player.getName() + " is facing: " + facingDirection);
+        float forward = event.getPacket().getFloat().read(1);
+        // Only proceed if the W key is pressed
+        if (forward <= 0) {
+            return;
+        }
 
-                // Update direction history
-                LinkedList<Vector> directions = lastTwoDirections.getOrDefault(player, new LinkedList<>());
-                directions.addLast(newDirection);
-                if (directions.size() > 2) {
-                    directions.removeFirst();
-                }
-                lastTwoDirections.put(player, directions);
+        float yaw = player.getLocation().getYaw();
+        yaw = (yaw % 360 + 360) % 360;  // Normalize yaw to [0, 360)
 
-                // Check for U-turn
-                if (isUTurn(player)) {
-                    gameManager.notifyUTurn(player);
-                }
-            }
+        Vector newDirection;
+        String facingDirection;
+        DebugManager.log(DebugManager.Category.PLAYER_INPUT, "Handling key press for player: " + event.getPlayer().getName());
+
+        // Determine the new direction based on player's yaw
+        if (yaw >= 315 || yaw < 45) {
+            newDirection = new Vector(0, 0, 1);  // South
+            facingDirection = "South";
+        } else if (yaw >= 45 && yaw < 135) {
+            newDirection = new Vector(-1, 0, 0);  // East
+            facingDirection = "East";
+        } else if (yaw >= 135 && yaw < 225) {
+            newDirection = new Vector(0, 0, -1);  // North
+            facingDirection = "North";
+        } else {  // yaw >= 225 && yaw < 315
+            newDirection = new Vector(1, 0, 0);  // West
+            facingDirection = "West";
+        }
+
+        playerDirections.put(player, newDirection);
+        DebugManager.log(DebugManager.Category.PLAYER_INPUT, "Player: " + player.getName() + " is facing: " + facingDirection);
+
+        // Update direction history
+        LinkedList<Vector> directions = lastTwoDirections.getOrDefault(player, new LinkedList<>());
+        directions.addLast(newDirection);
+        if (directions.size() > 2) {
+            directions.removeFirst();
+        }
+        lastTwoDirections.put(player, directions);
+
+        // Check for U-turns
+        if (isUTurn(player)) {
+            gameManager.notifyUTurn(player);
         }
     }
 
@@ -155,6 +170,8 @@ public class PlayerInputHandler {
         }
         Vector last = directions.getLast();
         Vector secondLast = directions.get(directions.size() - 2);
+
+        // Check if the last direction is the opposite of the second last
         return last.clone().multiply(-1).equals(secondLast);
     }
 }
