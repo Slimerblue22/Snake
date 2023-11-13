cb51a1dc7d8c43acc9ff00d175e3d8bf4f85ee55
Slimer <legodude063@gmail.com>
2023-11-12 21:20:55 -0500

    Update checker
    Runs once at startup

--- src/main/java/com/slimer/Main/Main.java
+++ src/main/java/com/slimer/Main/Main.java
@@ -13,7 +13,13 @@ import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -50,6 +56,7 @@ public final class Main extends JavaPlugin {
         initPlayerData();
         initMetrics();
         registerCommands();
+        checkForUpdates();
     }
 
     /**
@@ -126,6 +133,60 @@ public final class Main extends JavaPlugin {
         Objects.requireNonNull(getCommand("snakedebug")).setExecutor(new DebugManager.ToggleDebugCommand());
         Objects.requireNonNull(getCommand("snakegame")).setExecutor(new GameCommandHandler(gameManager, this));
         Objects.requireNonNull(getCommand("snakeregion")).setExecutor(new RegionCommandHandler());
+    }
+
+    /**
+     * Checks for updates by querying the latest release from the GitHub repository.
+     * It compares the current plugin version with the latest available version on GitHub
+     * and logs information about the update status.
+     */
+    private void checkForUpdates() {
+        Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
+            HttpURLConnection connection = null;
+            try {
+                URL url = new URL("https://api.github.com/repos/Slimerblue22/Snake/releases/latest");
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("Accept", "application/json");
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode != HttpURLConnection.HTTP_OK) {
+                    getLogger().warning("Update checker received non-OK response from GitHub: " + responseCode);
+                    return;
+                }
+
+                // Read response into string
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                    String inputLine;
+                    StringBuilder response = new StringBuilder();
+
+                    while ((inputLine = reader.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+
+                    // Remove the `V` in the GitHub version then compare
+                    JSONParser parser = new JSONParser();
+                    JSONObject jsonResponse = (JSONObject) parser.parse(response.toString());
+                    String latestVersion = ((String) jsonResponse.get("tag_name")).replaceFirst("^V", "");
+
+                    // Log and compare versions
+                    getLogger().info("Current plugin version: " + pluginVersion); // DEV BUILD ONLY
+                    getLogger().info("Latest available version: " + latestVersion); // DEV BUILD ONLY
+
+                    if (!pluginVersion.equalsIgnoreCase(latestVersion)) {
+                        getLogger().info("An update is available for the plugin. Current: " + pluginVersion + " Latest: " + latestVersion);
+                    } else {
+                        getLogger().info("Plugin is up to date.");
+                    }
+                }
+            } catch (Exception e) {
+                getLogger().severe("An error occurred while checking for updates: " + e.getMessage());
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+        });
     }
 
     /**
