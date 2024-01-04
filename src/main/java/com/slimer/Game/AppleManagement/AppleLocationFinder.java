6ee9d789928703ff65291074b217959c7406b874
Slimer <legodude063@gmail.com>
2024-01-04 17:01:23 -0500

    Some more location checks

--- src/main/java/com/slimer/Game/AppleManagement/AppleLocationFinder.java
+++ src/main/java/com/slimer/Game/AppleManagement/AppleLocationFinder.java
@@ -11,9 +11,12 @@ import org.bukkit.block.Block;
 
 import java.util.Random;
 
-// TODO: Hook this up with A* to ensure spots can be reached
+// TODO: Hook this up with A* to ensure spots can be reached (Or maybe BFS would be better?)
 public class AppleLocationFinder {
     private static final String LOCATION_FOUND_LOG = "Location found after %d attempts: %s";
+    private static final String FAILED_SOLID_BASE_CHECK_LOG = "Failed solid base check at %s - Block type: %s";
+    private static final String FAILED_AIR_SURROUNDING_CHECK_LOG = "Failed air surrounding check at %s - Block type: %s";
+    private static final String FAILED_AIR_ABOVE_CHECK_LOG = "Failed air above check at %s - Block type: %s";
     private static final int maxAttempts = 100;
 
     public AppleLocationFinder() {
@@ -42,12 +45,53 @@ public class AppleLocationFinder {
             int z = min.getZ() + random.nextInt(max.getZ() - min.getZ() + 1);
             randomLocation = new Location(world, x, fixedY, z);
 
-            Block block = randomLocation.getBlock();
-            if (block.getType() == Material.AIR) {
+            if (isSolidBase(randomLocation) && isAirSurrounding(randomLocation) && isAirAbove(randomLocation)) {
                 DebugManager.log(DebugManager.Category.DEBUG, String.format(LOCATION_FOUND_LOG, attemptCount, randomLocation));
                 return randomLocation; // Found a suitable location
             }
         }
         return null; // No suitable location found after maximum attempts
+    }
+
+    private boolean isSolidBase(Location location) {
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dz = -1; dz <= 1; dz++) {
+                Block block = location.clone().add(dx, -1, dz).getBlock();
+                if (!block.getType().isSolid()) {
+                    DebugManager.log(DebugManager.Category.DEBUG,
+                            String.format(FAILED_SOLID_BASE_CHECK_LOG, block.getLocation(), block.getType()));
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isAirSurrounding(Location location) {
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dz = -1; dz <= 1; dz++) {
+                Block block = location.clone().add(dx, 0, dz).getBlock();
+                if (block.getType() != Material.AIR) {
+                    DebugManager.log(DebugManager.Category.DEBUG,
+                            String.format(FAILED_AIR_SURROUNDING_CHECK_LOG, block.getLocation(), block.getType()));
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isAirAbove(Location location) {
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dz = -1; dz <= 1; dz++) {
+                Block block = location.clone().add(dx, 1, dz).getBlock();
+                if (block.getType() != Material.AIR) {
+                    DebugManager.log(DebugManager.Category.DEBUG,
+                            String.format(FAILED_AIR_ABOVE_CHECK_LOG, block.getLocation(), block.getType()));
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
